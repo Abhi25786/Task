389a3fc5cc2a7d895da8d8c00922fb4a1d890025
admin <admin@admins-iMac-4.local>
2022-04-20 19:00:24 +0530

    Work done

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
   },
   inputtext: {
     paddingVertical: moderateScaleVertical(10),
-
+    color: 'black',
     fontFamily: fontfamily.Mulish_Medium,
   },
 });
